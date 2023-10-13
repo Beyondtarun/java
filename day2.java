c98d17163b0bf018e97af2e087f0c96d0958be1e
Beyondtarun <teg.tarun@gmail.com>
2023-10-13 14:43:48 +0530

    update

--- day2.java
+++ day2.java
@@ -14,6 +14,7 @@ public static void main(String[] args) {
     // }
     // sum = (n*(n+1))/2;
     // System.out.println("Sum of n is : "+sum);
-    System.out.println("char at 0th index is: "+n.charAt(5));
+    System.out.println("char at 0th index is: "+n.charAt(5)); //print character
+    System.out.println("unicode valye at 0th index is: "+(int)n.charAt(5));//print unicode
 }
 }
