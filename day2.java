c675b1a1eeb246d3538df2bfb9dedaebdfd46a4c
Beyondtarun <teg.tarun@gmail.com>
2023-10-13 14:38:41 +0530

    update

--- day2.java
+++ day2.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class day2 {
 public static void main(String[] args) {
-    int sum =0;
+    // int sum =0;
     Scanner sc = new Scanner(System.in);
-    System.out.println("enter number: ");
-    int n = sc.nextInt();
+    System.out.println("enter string: ");
+    String n = sc.nextLine();
 
     // for (int i =1;i<=n;i++){
     //   sum +=i;
     // }
-    sum = (n*(n+1))/2;
-    System.out.println("Sum of n is : "+sum);
+    // sum = (n*(n+1))/2;
+    // System.out.println("Sum of n is : "+sum);
+    System.out.println("char at 0th index is: "+n.charAt(5));
 }
 }
