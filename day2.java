814b6b838197384f482c28b8bf6de8565f58dc81
Beyondtarun <teg.tarun@gmail.com>
2023-10-18 21:54:48 +0530

    update

--- day2.java
+++ day2.java
@@ -5,16 +5,28 @@ import java.util.Scanner;
 public class day2 {
 public static void main(String[] args) {
     // int sum =0;
-    Scanner sc = new Scanner(System.in);
-    System.out.println("enter string: ");
-    String n = sc.nextLine();
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("enter string: ");
+    // String n = sc.nextLine();
 
     // for (int i =1;i<=n;i++){
     //   sum +=i;
     // }
     // sum = (n*(n+1))/2;
     // System.out.println("Sum of n is : "+sum);
-    System.out.println("char at 0th index is: "+n.charAt(5)); //print character
-    System.out.println("unicode valye at 0th index is: "+(int)n.charAt(5));//print unicode
+    // System.out.println("char at 0th index is: "+n.charAt(5)); //print character
+    // System.out.println("unicode valye at 0th index is: "+(int)n.charAt(5));//print 
+    // System.out.println("unicode valye at 0th index is: "+ n.codePointAt(5));//print 
+    // System.out.println();
+
+    // String str = new String("hello");
+    // String str2 = new String("hello");
+    // str = "hello";
+    // str2 ="hello";
+
+    // System.out.println(str == str2);
+    String  str = null;
+    str = str.concat("java");
+    System.out.println(str);
 }
 }
