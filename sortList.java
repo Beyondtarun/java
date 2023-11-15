a6274e8ab1d1bfab03a4b6120076a444a0c525f5
Beyondtarun <teg.tarun@gmail.com>
2023-11-15 18:39:48 +0530

    new addition

---
+++ sortList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class sortList {
+    public static void main(String[] args) {
+        List<Integer> list =  new ArrayList<Integer>();
+        list.add(111);
+        list.add(31);
+        list.add(28);
+        list.add(12);
+        list.add(24);
+        list.add(20);
+        list.add(3);
+        System.err.println(list);
+        Collections.sort(list);
+System.err.println(list);
+
+    }
+}
