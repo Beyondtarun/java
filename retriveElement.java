d758c65451330136fec6c3312e70a87cacdedd6f
Beyondtarun <teg.tarun@gmail.com>
2023-11-15 10:55:35 +0530

    new addition

---
+++ retriveElement.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class retriveElement {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.err.println(list.get(2));
+    }
+}
