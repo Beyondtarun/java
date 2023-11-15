ad73ab1cc5bf415eabda40496428a4bb4004f327
Beyondtarun <teg.tarun@gmail.com>
2023-11-15 18:51:53 +0530

    updates

--- ClosestString.java
+++ ClosestString.java
@@ -1,32 +1,32 @@
-public class ClosestString {
+// public class ClosestString {
 	
-static int shortestDistance(String[] s,
-					String word1, String word2)
-{
-	int d1 = -1, d2 = -1;
-	int ans = Integer.MAX_VALUE;
+// static int shortestDistance(String[] s,
+// 					String word1, String word2)
+// {
+// 	int d1 = -1, d2 = -1;
+// 	int ans = Integer.MAX_VALUE;
 
-	for (int i = 0; i < s.length; i++) {
-		if (s[i] == word1)
-			d1 = i;
-		if (s[i] == word2)
-			d2 = i;
-		if (d1 != -1 && d2 != -1)
-			ans = Math.min(ans, Math.abs(d1 - d2));
-	}
+// 	for (int i = 0; i < s.length; i++) {
+// 		if (s[i] == word1)
+// 			d1 = i;
+// 		if (s[i] == word2)
+// 			d2 = i;
+// 		if (d1 != -1 && d2 != -1)
+// 			ans = Math.min(ans, Math.abs(d1 - d2));
+// 	}
 
-	// Return the answer
-	return ans;
-}
+// 	// Return the answer
+// 	return ans;
+// }
 
-// Driver Code
-	public static void main (String[] args) {
-		String[] S
-		= { "the", "quick", "brown", "fox", "quick" };
+// // Driver Code
+// 	public static void main (String[] args) {
+// 		String[] S
+// 		= { "the", "quick", "brown", "fox", "quick" };
 
-	String word1 = "the", word2 = "fox";
+// 	String word1 = "the", word2 = "fox";
 
-	// Function Call
-	System.out.println(shortestDistance(S, word1, word2));
-	}
-}
+// 	// Function Call
+// 	System.out.println(shortestDistance(S, word1, word2));
+// 	}
+// }
--- DataSizeRange.java
+++ DataSizeRange.java
@@ -1,10 +1,10 @@
-import java.lang.*;
+// import java.lang.*;
 
-class DataSizeRange{
-              public static void main(String args[]){
+// class DataSizeRange{
+//               public static void main(String args[]){
 
-System.out.println("Int Min value: " + Integer.MIN_VALUE);
-System.out.println("Int max value: " + Integer.MAX_VALUE);
-              System.out.println("Int bytes: " + Integer.BYTES);
-}
-}
+// System.out.println("Int Min value: " + Integer.MIN_VALUE);
+// System.out.println("Int max value: " + Integer.MAX_VALUE);
+//               System.out.println("Int bytes: " + Integer.BYTES);
+// }
+// }
