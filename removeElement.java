c84cb4fd8084ec7acfb836858ed2b254b4642c07
Beyondtarun <teg.tarun@gmail.com>
2023-11-15 11:31:11 +0530

    updates

--- removeElement.java
+++ removeElement.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.util.Iterator;
 
 public class removeElement {
     public static void main(String[] args) {
@@ -7,8 +10,27 @@ public class removeElement {
         list.add(2);
         list.add(3);
         System.err.println(list);
-        list.remove(2);
-        System.err.println(list);
+        Scanner sc = new Scanner(System.in);
+        System.err.println("enter number to be searched:");
+        int item = sc.nextInt();
+        // Iterator<Integer> iterator =  list.iterator();
+        // int flag =0;
+        // while(iterator.hasNext()){
+        //     if(item == iterator.next())
+        // flag =1;    
+        // }
+        
+        // if(flag ==1){
+        //     System.err.println("found");
+        // }
+        // else{
+        //     System.err.println("not found");
+        // }
+        if(list.contains(item))
+        System.err.println("element fount");
+        else{
+            System.err.println("not founds");
+        }
     }
 
 }
