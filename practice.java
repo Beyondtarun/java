58f03cb617501c5ef3f68681480192eaa514d92a
Beyondtarun <teg.tarun@gmail.com>
2023-11-19 18:28:46 +0530

    update

--- practice.java
+++ practice.java
@@ -1,26 +1,21 @@
-public class practice {
-    // public static void main(String[] args) {
-    //     // System.err.println('a'+'b');
-    //     int $_ = 10;
-    // }
-
-    public static void main(String[] arr){ 
-        Integer num1 = 200; 
-        Integer num2 = 200; 
-        Integer num3 = 500; 
-        Integer num4 = 500; 
-          
-        if(num1==num2){ 
-            System.out.println("num1 == num2"); 
-        } 
-        else{ 
-            System.out.println("num1 != num2"); 
-        } 
-        if(num3==num4){ 
-            System.out.println("num3 == num4"); 
-        } 
-        else{ 
-            System.out.println("num3 != num4"); 
-        } 
-    } 
-}
+class Person{  
+    int id;  
+    String name;  
+    Person(int id,String name){  
+    this.id=id;  
+    this.name=name;  
+    }  
+    }  
+    class Emp extends Person{  
+    float salary;  
+    Emp(int id,String name,float salary){  
+    super(id,name);//reusing parent constructor  
+    this.salary=salary;  
+    }  
+    void display(){System.out.println(id+" "+name+" "+salary);}  
+    }  
+    class practice{  
+    public static void main(String[] args){  
+    Emp e1=new Emp(1,"ankit",45000f);  
+    e1.display();  
+    }}  
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+erson(int id1, String name1) {
+    //     this.id = id1;
+    //     this.name = name1;
+    // }
