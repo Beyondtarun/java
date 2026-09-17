9e85b5bcf94ea2705575e4d89b850e5953d7c070
beyondtarun <teg.tarun@gmail.com>
2026-09-17 23:35:17 +0530

    executions

--- codes/arithmeticProgression.java
+++ codes/arithmeticProgression.java
@@ -6,22 +6,22 @@
 //  *
 //  * @author Tarun Goyal
 //  */
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class arithmeticProgression {
-//     public static void main(String args[]){
-//         Scanner sc=new Scanner(System.in);
-//         System.out.println("ENTER FIRST DIGIT OF AP: ");
-//         int a=sc.nextInt();
-//         System.out.println("ENTER DIFFERENCE: ");
-//         int d=sc.nextInt();
-//         System.out.println("ENTER NUMBER OF DIGITS: ");
-//         int n=sc.nextInt();
-//        System.out.println("AP IS: ");
+public class arithmeticProgression {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER FIRST DIGIT OF AP: ");
+        int a=sc.nextInt();
+        System.out.println("ENTER DIFFERENCE: ");
+        int d=sc.nextInt();
+        System.out.println("ENTER NUMBER OF DIGITS: ");
+        int n=sc.nextInt();
+       System.out.println("AP IS: ");
             
-//         for(int i=1;i<=n;i++){
-//             System.out.print(a+" ");
-//             a=a+d;
-//         }
-//     }
-// }
+        for(int i=1;i<=n;i++){
+            System.out.print(a+" ");
+            a=a+d;
+        }
+    }
+}
